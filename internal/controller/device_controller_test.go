b82c24bdbe7532c132623bcc2bf0f7c24f1e64c5
Felix Kästner <felix.kaestner@sap.com>
2025-08-15 16:22:07 +0200

    Add finalizer to referenced endpoint credentials secret
    This prevents the deletion of the secret, while the device is still in
    use.

--- internal/controller/device_controller_test.go
+++ internal/controller/device_controller_test.go
@@ -6,9 +6,10 @@ package controller
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -19,17 +20,33 @@ import (
 var _ = Describe("Device Controller", func() {
 	Context("When reconciling a resource", func() {
 		const name = "test-device"
-		key := types.NamespacedName{Name: name, Namespace: metav1.NamespaceDefault}
+		key := client.ObjectKey{Name: name, Namespace: metav1.NamespaceDefault}
+
+		const secretName = "test-device-credentials"
+		secretKey := client.ObjectKey{Name: secretName, Namespace: metav1.NamespaceDefault}
 
 		BeforeEach(func() {
+			By("Creating the endpoint credentials as a Secret")
+			secret := &corev1.Secret{}
+			if err := k8sClient.Get(ctx, secretKey, secret); errors.IsNotFound(err) {
+				resource := &corev1.Secret{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      secretName,
+						Namespace: metav1.NamespaceDefault,
+					},
+					Data: map[string][]byte{
+						corev1.BasicAuthUsernameKey: []byte("user"),
+						corev1.BasicAuthPasswordKey: []byte("password"),
+					},
+					Type: corev1.SecretTypeBasicAuth,
+				}
+				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+			}
+
 			By("Creating the custom resource for the Kind Device")
 			device := &v1alpha1.Device{}
 			if err := k8sClient.Get(ctx, key, device); errors.IsNotFound(err) {
 				resource := &v1alpha1.Device{
-					TypeMeta: metav1.TypeMeta{
-						APIVersion: v1alpha1.GroupVersion.String(),
-						Kind:       "Device",
-					},
 					ObjectMeta: metav1.ObjectMeta{
 						Name:      name,
 						Namespace: metav1.NamespaceDefault,
@@ -37,6 +54,9 @@ var _ = Describe("Device Controller", func() {
 					Spec: v1alpha1.DeviceSpec{
 						Endpoint: &v1alpha1.Endpoint{
 							Address: "192.168.10.2:9339",
+							SecretRef: &corev1.SecretReference{
+								Name: "test-device-credentials",
+							},
 						},
 						Bootstrap: &v1alpha1.Bootstrap{
 							Template: &v1alpha1.TemplateSource{
@@ -50,12 +70,19 @@ var _ = Describe("Device Controller", func() {
 		})
 
 		AfterEach(func() {
-			resource := &v1alpha1.Device{}
-			err := k8sClient.Get(ctx, key, resource)
+			device := &v1alpha1.Device{}
+			err := k8sClient.Get(ctx, key, device)
 			Expect(err).NotTo(HaveOccurred())
 
 			By("Cleanup the specific resource instance Device")
-			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+			Expect(k8sClient.Delete(ctx, device)).To(Succeed())
+
+			secret := &corev1.Secret{}
+			err = k8sClient.Get(ctx, secretKey, secret)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Cleanup the specific resource instance Secret")
+			Expect(k8sClient.Delete(ctx, secret)).To(Succeed())
 
 			By("Ensuring the resource is deleted from the provider")
 			Eventually(func(g Gomega) {
@@ -69,6 +96,13 @@ var _ = Describe("Device Controller", func() {
 			Eventually(func(g Gomega) {
 				resource := &v1alpha1.Device{}
 				g.Expect(k8sClient.Get(ctx, key, resource)).To(Succeed())
+				g.Expect(controllerutil.ContainsFinalizer(resource, v1alpha1.FinalizerName)).To(BeTrue())
+			}).Should(Succeed())
+
+			By("Adding a finalizer to the credentials secret")
+			Eventually(func(g Gomega) {
+				resource := &corev1.Secret{}
+				g.Expect(k8sClient.Get(ctx, secretKey, resource)).To(Succeed())
 				g.Expect(controllerutil.ContainsFinalizer(resource, v1alpha1.FinalizerName)).To(BeTrue())
 			}).Should(Succeed())
 
