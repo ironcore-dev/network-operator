d34c54d8c8be017d2cabeb0014aa5c12c9e81539
Sebastian Wagner <sebastian.wagner02@sap.com>
2025-12-03 17:37:33 +0100

    Rename and refactor bootstrap, implement basic provisioning functions
    This renames `DeviceSpec.Bootstrap` to `Provisioning` in order to have a
    unified naming. We also implement a ProvisioningProvider interface and
    state transitionings in the device controller.

--- api/core/v1alpha1/groupversion_info.go
+++ api/core/v1alpha1/groupversion_info.go
@@ -39,6 +39,11 @@ const FinalizerName = "networking.metal.ironcore.dev/finalizer"
 // based on the device they are intended for.
 const DeviceLabel = "networking.metal.ironcore.dev/device-name"
 
+// DeviceSerialLabel is a label applied to any Network API object to indicate the serial number
+// of the device it is associated with. This label is used by the http provisioning server
+// to find the device when a provisioning request is received.
+const DeviceSerialLabel = "networking.metal.ironcore.dev/device-serial"
+
 // DeviceKind represents the Kind of Device.
 const DeviceKind = "Device"
 
@@ -75,6 +80,21 @@ const (
 	// intended (e.g., a interface is up). It corresponds to the "oper-status" commonly found
 	// in the OpenConfig models (or "operSt" on Cisco).
 	OperationalCondition = "Operational"
+)
+
+// ProvisioningReasonType represents the reason for the current provisioning status.
+type ProvisioningReasonType string
+
+const (
+	ProvisioningScriptExecutionStarted         ProvisioningReasonType = "ScriptExecutionStarted"
+	ProvisioningScriptExecutionFailed          ProvisioningReasonType = "ScriptExecutionFailed"
+	ProvisioningInstallingCertificates         ProvisioningReasonType = "InstallingCertificates"
+	ProvisioningDownloadingImage               ProvisioningReasonType = "DownloadingImage"
+	ProvisioningImageDownloadFailed            ProvisioningReasonType = "ImageDownloadFailed"
+	ProvisioningUpgradeStarting                ProvisioningReasonType = "UpgradeStarting"
+	ProvisioningUpgradeFailed                  ProvisioningReasonType = "UpgradeFailed"
+	ProvisioningRebootingDevice                ProvisioningReasonType = "RebootingDevice"
+	ProvisioningExecutionFinishedWithoutReboot ProvisioningReasonType = "ExecutionFinishedWithoutReboot"
 )
 
 // Reasons that are used across different objects.
