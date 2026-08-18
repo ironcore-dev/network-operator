d4e15c01ad779b685ab1ba33632009fc46125cb7
Robert Gildein <rgildein@users.noreply.github.com>
2026-08-18 15:18:42 +0200

    fix ServerName rule
    Signed-off-by: Robert Gildein <rgildein@users.noreply.github.com>

--- api/core/v1alpha1/managementaccess_types.go
+++ api/core/v1alpha1/managementaccess_types.go
@@ -57,7 +57,7 @@ type GRPC struct {
 	// Not supported on Cisco NX-OS devices.
 	// Immutable once set.
 	// +optional
-	// +kubebuilder:validation:XValidation:rule="!has(oldSelf) || self == oldSelf",message="ServerName is immutable"
+	// +kubebuilder:validation:XValidation:rule="oldSelf == '' || self == oldSelf",message="ServerName is immutable"
 	// +kubebuilder:validation:MinLength=1
 	// +kubebuilder:validation:MaxLength=63
 	ServerName string `json:"serverName,omitempty"`
